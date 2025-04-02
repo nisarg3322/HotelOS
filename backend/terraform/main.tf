d8172476373f6c61c9087c3f3cd85499b8996d49
Nisarg Patel <patelnisarg.3322@gmail.com>
2025-04-02 09:05:56 -0400

    added elastic IP for ec2 instance

--- backend/terraform/main.tf
+++ backend/terraform/main.tf
@@ -84,22 +84,6 @@ resource "aws_security_group" "rds_sg" {
   }
 }
 
-# Generate SSH Key Pair
-resource "tls_private_key" "app_private_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "aws_key_pair" "app_key_pair" {
-  key_name   = "study-sync-key"
-  public_key = tls_private_key.app_private_key.public_key_openssh
-}
-
-resource "local_sensitive_file" "app_private_key_file" {
-  content         = tls_private_key.app_private_key.private_key_pem
-  filename        = "${path.module}/${aws_key_pair.app_key_pair.key_name}.pem"
-  file_permission = "400"
-}
 
 # IAM Role for EC2 with SecretsManager permissions
 resource "aws_iam_role" "ec2_secrets_role" {
@@ -134,157 +118,17 @@ resource "aws_iam_role_policy" "secrets_manager_access" {
   })
 }
 
-# resource "aws_instance" "app_server" {
-#   ami                    = data.aws_ami.latest_amazon_linux.id
-#   instance_type          = "t3.micro"
-#   key_name               = aws_key_pair.app_key_pair.key_name
-#   vpc_security_group_ids = [aws_security_group.app_sg.id]  # EC2 SG
 
-#   # user_data = <<-EOF
-#   #             #!/bin/bash
-              
-#   #             echo "Updating system..."
-#   #             sudo yum update -y
 
-#   #             echo "Installing Git..."
-#   #             sudo yum install git -y
 
-#   #             echo "Cloning the repository..."
-#   #             git clone https://github.com/nisarg3322/e-hotel.git /home/ec2-user/e-hotel
-
-#   #             echo "Installing Node.js..."
-#   #             curl -sL https://rpm.nodesource.com/setup_lts.x | sudo bash -
-#   #             sudo yum install -y nodejs
-
-#   #             echo "Installing PostgreSQL client..."
-#   #             sudo yum install postgresql15 -y
-
-#   #             echo "Changing directory to backend..."
-#   #             cd /home/ec2-user/e-hotel/backend
-
-#   #             echo "Installing dependencies..."
-#   #             npm install
-
-#   #             echo "Running database initialization..."
-#   #             export PGPASSWORD="StrongPassword123!"
-#   #             psql -h ${aws_db_instance.postgres.address} -U nisarg -d mydatabase -p 5432 -f /home/ec2-user/e-hotel/backend/db-init/init.sql
-
-#   #             echo "Starting Node.js server..."
-#   #             node server.js
-#   #             EOF
-#   user_data = <<-EOF
-#             #!/bin/bash
-#             echo "Updating system..."
-#             sudo yum update -y
-
-#             echo "Installing AWS CLI..."
-#             sudo yum install -y aws-cli
-
-#             echo "Fetching database credentials from AWS Secrets Manager..."
-#             DB_CREDENTIALS=$(aws secretsmanager get-secret-value --secret-id study-sync-db-credentials --query SecretString --output text --region us-east-1)
-#             DB_USER=$(echo $DB_CREDENTIALS | jq -r '.DB_USER')
-#             DB_PASSWORD=$(echo $DB_CREDENTIALS | jq -r '.DB_PASSWORD')
-
-#             echo "Installing Git..."
-#             sudo yum install git -y
-
-#             echo "Cloning repository..."
-#             git clone https://github.com/nisarg3322/e-hotel.git /home/ec2-user/e-hotel
-
-#             echo "Installing Node.js..."
-#             curl -sL https://rpm.nodesource.com/setup_lts.x | sudo bash -
-#             sudo yum install -y nodejs
-
-#             echo "Installing PostgreSQL client..."
-#             sudo yum install postgresql15 -y
-
-#             echo "Changing directory to backend..."
-#             cd /home/ec2-user/e-hotel/backend
-
-#             echo "Creating .env file..."
-#             cat <<EOT | sudo tee /home/ec2-user/e-hotel/backend/.env
-#             DB_HOST=${aws_db_instance.postgres.address}
-#             DB_USER=$DB_USER
-#             DB_PASSWORD=$DB_PASSWORD
-#             DB_NAME=mydatabase
-#             DB_PORT=5432
-#             PORT=3000
-#             EOT
-
-#             echo "Setting permissions for .env..."
-#             sudo chmod 600 /home/ec2-user/e-hotel/backend/.env
-
-#             echo "Installing dependencies..."
-#             npm install
-
-#             echo "Starting server..."
-#             node server.js
-#             EOF
-#   tags = {
-#     Name = "study-sync-app-server"
-#   }
-# }
 
 resource "aws_instance" "app_server" {
   ami                    = data.aws_ami.latest_amazon_linux.id
   instance_type          = "t3.micro"
-  key_name               = aws_key_pair.app_key_pair.key_name
+  key_name               = "e-hotels"
   vpc_security_group_ids = [aws_security_group.app_sg.id]
   iam_instance_profile   = aws_iam_instance_profile.ec2_instance_profile.id  # Attach the IAM role
 
-  # user_data = <<-EOF
-  #             #!/bin/bash
-  #           echo "Updating system..."
-  #           sudo yum update -y
-
-  #           echo "Installing AWS CLI..."
-  #           sudo yum install aws-cli -y
-
-  #           echo "Fetching database credentials from AWS Secrets Manager..."
-  #           secret=$(aws secretsmanager get-secret-value --secret-id study-sync-db-credentials --query SecretString --output text --region us-east-1)
-
-  #           DB_USER=$(echo $secret | jq -r '.DB_USER')
-  #           DB_PASSWORD=$(echo $secret | jq -r '.DB_PASSWORD')
-
-  #           echo "Installing Git..."
-  #           sudo yum install git -y
-
-  #           echo "Cloning repository..."
-  #           git clone https://github.com/nisarg3322/e-hotel.git /home/ec2-user/e-hotel
-
-  #           echo "Installing Node.js..."
-  #           curl -sL https://rpm.nodesource.com/setup_lts.x | sudo bash -
-  #           sudo yum install nodejs -y
-
-  #           echo "Installing PostgreSQL client..."
-  #           sudo yum install postgresql15 -y
-
-  #           echo "Changing directory to backend..."
-  #           cd /home/ec2-user/e-hotel/backend
-
-  #           echo "Creating .env file..."
-  #           cat <<EOT | sudo tee /home/ec2-user/e-hotel/backend/.env
-  #           DB_HOST=${aws_db_instance.postgres.address}
-  #           DB_USER=$DB_USER
-  #           DB_PASSWORD=$DB_PASSWORD
-  #           DB_NAME=mydatabase
-  #           DB_PORT=5432
-  #           PORT=3000
-  #           EOT
-
-  #           echo "Setting permissions for .env..."
-  #           sudo chmod 600 /home/ec2-user/e-hotel/backend/.env
-
-  #           echo "Installing dependencies..."
-  #           npm install
-
-  #           echo "Running database initialization..."
-  #           export PGPASSWORD="StrongPassword123!"
-  #           psql -h ${aws_db_instance.postgres.address} -U nisarg -d mydatabase -p 5432 -f /home/ec2-user/e-hotel/backend/db-init/init.sql
-
-  #           echo "Starting server..."
-  #           node server.js
-  #           EOF
 
   user_data = <<EOF
 #!/bin/bash
@@ -310,6 +154,7 @@ git clone https://github.com/nisarg3322/e-hotel.git /home/ec2-user/e-hotel
 echo "Installing Node.js..."
 curl -sL https://rpm.nodesource.com/setup_lts.x | sudo bash -
 sudo yum install nodejs -y
+npm install pm2@latest -g
 
 echo "Installing PostgreSQL client..."
 sudo yum install postgresql15 -y
@@ -338,13 +183,34 @@ export PGPASSWORD="StrongPassword123!"
 psql -h ${aws_db_instance.postgres.address} -U nisarg -d mydatabase -p 5432 -f /home/ec2-user/e-hotel/backend/db-init/init.sql
 
 echo "Starting server..."
-node server.js > /home/ec2-user/e-hotel/backend/server.log 2>&1 &
+pm2 start server.js --name e-hotel
+
+# Save the PM2 process list
+pm2 save
+
+# Configure PM2 to restart on system reboot
+pm2 startup systemd
 EOF
 
 
   tags = {
     Name = "study-sync-app-server"
   }
+
+    associate_public_ip_address = false
+
+}
+
+data "aws_eip" "existing_eip" {
+  filter {
+    name   = "tag:name"
+    values = ["e-hotels-ip"]  # Match the tag of your manually created EIP
+  }
+}
+
+resource "aws_eip_association" "app_eip_assoc" {
+  instance_id   = aws_instance.app_server.id
+  allocation_id = data.aws_eip.existing_eip.id
 }
 
 # Instance Profile for EC2 to attach the IAM Role
